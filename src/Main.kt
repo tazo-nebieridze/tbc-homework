34ea59a5243de347b25b5b9db8bbff50d85ba3be
tazo nebieridze <tazoneb1@gmail.com>
2024-11-27 18:13:09 +0400

    numero sete

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
 
-    print(73)
+fun getNumbers(input:String):String {
+
+    return input.filter { c: Char -> c.isDigit() }
 
 }
+
+
+fun main() {
+    while (true) {
+        println("შეიყვანეთ X ცვლადის მნიშვნელობა:")
+        val x = readLine() ?: ""
+
+        println("შეიყვანეთ ცვლადის მნიშვნელობა Y:")
+        val y = readLine() ?: ""
+
+        val xNumbers = getNumbers(x)
+        val yNumbers = getNumbers(y)
+
+        var xValue = 0
+
+        if (!xNumbers.isEmpty()){
+
+            xValue = xNumbers.toInt()
+
+        }
+
+        var yValue = 0
+
+        if (!yNumbers.isEmpty()){
+
+            yValue = yNumbers.toInt()
+
+        }
+
+        var result: String;
+
+        if (yValue == 0) {
+            result = "ნულზე გაყოფა აკრძალულია"
+        }  else {
+            val z = xValue / yValue
+            result = "X და Y განაყოფი არის: $z"
+        }
+
+        println(result)
+
+        var answer: String;
+
+        while (true) {
+            println("გსურთ პროგრამის ხელახლა დაწყება <Y/N>?")
+            answer = readLine()?.trim() ?: ""
+            if (answer == "y" || answer == "n" || answer == "Y" || answer == "N") {
+                break
+            } else {
+                println("გთხოვთ შეიყვანოთ მხოლოდ \"Y\" ან \"N\"")
+            }
+        }
+
+
+        if (answer == "n" || answer == "N") {
+            break
+        }
+    }
+}
+
