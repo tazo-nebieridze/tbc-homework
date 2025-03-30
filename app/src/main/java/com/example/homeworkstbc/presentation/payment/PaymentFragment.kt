9994c008b4c33b88da4c0202587f89b3ff7b149f
tazo nebieridze <tazoneb1@gmail.com>
2025-03-30 22:59:09 +0400

    davaleba_27 only UI

---
+++ app/src/main/java/com/example/homeworkstbc/presentation/payment/PaymentFragment.kt
@@ -0,0 +1,18 @@
+package com.example.homeworkstbc.presentation.payment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.homeworkstbc.R
+import com.example.homeworkstbc.databinding.FragmentPaymentBinding
+import com.example.homeworkstbc.presentation.base.BaseFragment
+
+class PaymentFragment : BaseFragment<FragmentPaymentBinding>(FragmentPaymentBinding::inflate) {
+
+    override fun start() {
+
+    }
+
+}
